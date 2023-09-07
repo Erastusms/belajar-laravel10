743154b3ce989a9d53b927d346e8fab42179f8b8
Erastus Mauliate <emauliate17@gmail.com>
2023-09-07 11:36:41 +0700

    add Contact API

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
@@ -14,6 +15,13 @@ class Contact extends Model
     public $incrementing = true;
     public $timestamps = true;
 
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+    ];
+    
     public function user(): BelongsTo
     {
         return $this->belongsTo(Contact::class, "user_id", "id");
